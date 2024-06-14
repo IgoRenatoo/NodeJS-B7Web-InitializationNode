e176f0f5a68c157a42975e8bf832e43c0e5cc968
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-06-13 22:44:56 -0300

    feat: :sparkles: Adicionado sincronização com DataBase MySQL

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,7 +1,14 @@
 import { displayItem } from './../models/product';
 import { Request , Response } from "express";
+import { sequelize } from './../instances/mysql';
 
-export const mainHome = (req: Request, res: Response) => {
+export const mainHome = async (req: Request, res: Response) => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão estabelecida com sucesso!')
+  } catch (error) {
+    console.log(`Houve um error: ${error}`)
+  }
   let produtos = displayItem.getAll();
   res.render('./pages/home', {
     produtos    
